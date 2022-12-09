fa586599063913e8b86ea5a076182476b7b3a265
obouhlel Home <oussama_bouhlel@outlook.fr>
2022-12-09 11:59:39 +0100

    do the tester

--- push_swap_algo.c
+++ push_swap_algo.c
@@ -6,7 +6,7 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 17:01:56 by obouhlel          #+#    #+#             */
-/*   Updated: 2022/12/09 09:52:38 by obouhlel         ###   ########.fr       */
+/*   Updated: 2022/12/09 11:59:15 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 void	ft_algo_push_swap(t_list **lst_a, t_list **lst_b)
 {
-	// int	len;
+	int	len;
+	int	split_len;
+	int	i;
 
-	// len = ft_lstsize((*lst_a));
+	len = ft_lstsize((*lst_a));
 	if (ft_push_swap_check((*lst_a)))
 	{
 		ft_putendl_fd("SUCCESS", 1);
 		return ;
 	}
-	ft_push_swap_pa(lst_b, lst_a);
-	ft_print_color(*lst_a, *lst_b);
+	split_len = len / 5;
+	i = -1;
+	while (++i < split_len)
+		ft_push_swap_pa(lst_b, lst_a);
 }
