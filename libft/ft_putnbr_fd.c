528c9ecae9c637cfe4a04d305be63519417cf35a
obouhlel Home <oussama_bouhlel@outlook.fr>
2022-12-16 14:19:11 +0100

    ok

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 12:44:11 by obouhlel          #+#    #+#             */
+/*   Updated: 2022/12/15 21:00:51 by obouhlel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long int	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
+	else
+		ft_putchar_fd((nb + 48), fd);
+}
---
+++ test_push_swap.sh
@@ -0,0 +1,45 @@
+# Color
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Cyan='\033[0;36m'
+Purple='\033[0;35m'
+Color_Off='\033[0m'
+
+rm -f push_swap
+make
+make clean
+
+echo $Cyan "\nTeste\n" $Color_Off
+
+# 5.4
+ARG="5 4 3 2 1"
+echo $Cyan $ARG $Color_Off
+./push_swap $ARG
+
+# ARG="1 9 2 3 4 5 6"
+# echo $Cyan $ARG $Color_Off
+# ./push_swap $ARG
+
+# ARG="2 3 5 12 10 6 9 7 4 1 8 11"
+# echo $Cyan $ARG $Color_Off
+# ./push_swap $ARG
+
+# # 10
+# ARG="7 9 2 1 0 3 4 5 6 8"
+# echo $Cyan $ARG $Color_Off
+# ./push_swap $ARG
+
+# # 20
+# ARG="0 14 5 15 2 12 17 6 19 13 4 3 11 1 16 7 9 8 10 18"
+# echo $Cyan $ARG $Color_Off
+# ./push_swap $ARG
+
+# ARG="-253 904 -590 805 838 212 -50 247 430 336 131 -306 -368 -759 -403 -23 201 -72 480 690"
+# echo $Cyan $ARG $Color_Off
+# ./push_swap $ARG
+
+# # 100
+# ARG="94 76 84 83 56 2 22 5 74 57 12 43 27 82 77 53 16 50 3 64 14 34 19 91 11 72 21 51 1 32 61 89 70 69 47 86 79 68 15 24 59 49 17 63 35 29 92 78 54 18 42 10 9 6 55 73 97 44 39 65 30 62 52 99 60 33 81 4 95 0 37 36 80 75 31 13 7 8 67 58 66 98 23 93 41 20 96 38 85 48 71 45 28 90 46 40 88 26 25 87"
+# echo $Cyan $ARG $Color_Off
+# ./push_swap $ARG
