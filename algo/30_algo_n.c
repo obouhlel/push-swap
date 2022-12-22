2bf6d6c1a2b7fc0fae4a9cfbbf652ea9d3cc921b
obouhlel Home <oussama_bouhlel@outlook.fr>
2022-12-22 16:07:37 +0100

    need to do ft_algo_n_price

--- algo/30_algo_n.c
+++ algo/30_algo_n.c
@@ -6,19 +6,26 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 19:02:19 by obouhlel          #+#    #+#             */
-/*   Updated: 2022/12/22 13:35:04 by obouhlel         ###   ########.fr       */
+/*   Updated: 2022/12/22 16:06:17 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-// void	ft_algo_n_price(t_stack *stack_a, t_stack *stack_b, t_info *info)
-// {
-// 	int	id_a;
-// 	int	id_b;
-// }
+void	ft_algo_n_price(t_stack *stack_a, t_stack *stack_b, t_info *info)
+{
+	int	id_a;
+	int	id_b;
+
+	while ()
+	{
+		
+	}
+}
 
 void	ft_algo_n(t_info *info)
 {
-	ft_algo_lis(info);
+	ft_algo_n_med(info);
+	ft_algo_n_price(info->stack_a, info->stack_b, info);
+	ft_print_info(info);
 }
