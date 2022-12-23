74e88d45dd7741f67ce89933694e7b51c9e62f67
obouhlel Home <oussama_bouhlel@outlook.fr>
2022-12-23 21:20:40 +0100

    need norme and optimisation

--- algo/30_algo_n.c
+++ algo/30_algo_n.c
@@ -6,32 +6,11 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 19:02:19 by obouhlel          #+#    #+#             */
-/*   Updated: 2022/12/23 21:12:21 by obouhlel         ###   ########.fr       */
+/*   Updated: 2022/12/23 21:19:01 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-// void	ft_algo_n_price_end(t_stack *stack_a, t_stack *stack_b, t_info *info)
-// {
-// 	int	id_a;
-// 	int	id_b;
-
-// 	if (info->max_a > info->max_b)
-// 		info->max_b = info->max_a;
-// 	if (stack_b->value == info->max_b)
-// 	{
-// 		id_b = 0;
-// 		id_a = ft_algo_find_id(info->min_a, stack_a);
-// 		ft_calcule_best_combot(id_a, id_b, info);
-// 	}
-// 	else
-// 	{
-// 		id_b = 0;
-// 		id_a = ft_algo_find_next_pos(stack_b->pos, stack_a);
-// 		ft_calcule_best_combot(id_a, id_b, info);
-// 	}
-// }
 
 void	ft_algo_n_price(t_stack *stack_a, t_stack *stack_b, t_info *info)
 {
@@ -86,16 +65,12 @@ void	ft_algo_n(t_info *info)
 	int	i;
 
 	ft_algo_n_med(info);
-	i = 5;
 	while (info->size_b)
 	{
 		ft_algo_n_price(info->stack_a, info->stack_b, info);
 		ft_do_move(info);
 		ft_init_move_info(info);
 		ft_update_info(info);
-		i--;
 	}
 	ft_algo_n_sort(info);
-	// ft_stack_pos_init(info->stack_a, info->stack_b);
-	// ft_print_info(info);
 }
